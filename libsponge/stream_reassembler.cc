a4079d3b34c64a9d7b63e9b8d2901bd63833f61b
pumpkin <602437361@qq.com>
2023-04-23 20:04:32 +0800

    feat lab4 solution

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -12,15 +12,112 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {
+    _buffer.resize(capacity);
+}
+
+long StreamReassembler::merge_block(block_node &elm1, const block_node &elm2) {
+    block_node x, y;
+    if(elm1.begin >elm2.begin) {
+        x = elm2;
+        y = elm1;
+    } else {
+        x = elm1;
+        y = elm2;
+    }
+    //没有交集 不用merge 返回-1
+    if(x.begin + x.length < y.begin) {
+        return -1;
+    //x 覆盖了 y, 合并之后数据为x，合并长度也就是交集长度，为y的数据长度
+    } else if(x.begin + x.length >= y.begin + y.length) {
+        elm1 = x;
+        return y.length;
+    } else {
+        elm1.begin = x.begin;
+        elm1.data = x.data + y.data.substr(x.begin + x.length - y.begin);
+        elm1.length = elm1.data.length();
+        return x.begin + x.length - y.begin;
+    }
+}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    //缓存满了
+    if(index >= _head_index + _capacity) {
+        return;
+    }
+
+    block_node elm;
+    if(index + data.length() <= _head_index) {
+        goto JUDGE_EOF;
+    //子串前缀重叠
+    } else if(index < _head_index) {
+        size_t offset = _head_index - index;
+        elm.begin = index + offset;
+        elm.data.assign(data.begin() + offset, data.end());
+        elm.length = elm.data.length();
+    } else {
+        elm.begin = index;
+        elm.data = data;
+        elm.length = data.length();
+    }
+
+    _unassembler_byte = elm.length;
+
+    //子串重叠，合并子串
+    do {
+        long merged_bytes = 0;
+        auto iter = _blocks.lower_bound(elm);
+        while(iter != _blocks.end() && (merged_bytes = merge_block(elm, *iter)) >= 0) {
+            _unassembler_byte -= merged_bytes;
+            _blocks.erase(iter);
+            iter = _blocks.lower_bound(elm);
+        }
+
+        if(iter == _blocks.begin()) {
+            break;
+        }
+
+        iter--;
+        while((merged_bytes = merge_block(elm, *iter)) >= 0) {
+            _unassembler_byte -= merged_bytes;
+            _blocks.erase(iter);
+            iter = _blocks.lower_bound(elm);
+            if(iter == _blocks.begin()) {
+                break;
+            }
+            iter--;
+        }
+    }while(false);
+    _blocks.insert(elm);
+
+    //写入字节流
+    if(!_blocks.empty() && _blocks.begin()->begin == _head_index) {
+        const block_node head_node = *_blocks.begin();
+        size_t write_bytes = _output.write(head_node.data);
+        _head_index += write_bytes;
+        _unassembler_byte -= write_bytes;
+        _blocks.erase(_blocks.begin());
+    }
+
+JUDGE_EOF:
+    if(eof) {
+        _eof_flag = true;
+    }
+    if(_eof_flag && empty()) {
+        _output.end_input();
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
 
-bool StreamReassembler::empty() const { return {}; }
+
+size_t StreamReassembler::unassembled_bytes() const {
+    return _unassembler_byte;
+ }
+
+bool StreamReassembler::empty() const {
+    return _unassembler_byte == 0;
+}
+
--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -12,38 +12,155 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-size_t TCPConnection::remaining_outbound_capacity() const { return {}; }
+size_t TCPConnection::remaining_outbound_capacity() const { return _sender.stream_in().remaining_capacity(); }
 
-size_t TCPConnection::bytes_in_flight() const { return {}; }
+size_t TCPConnection::bytes_in_flight() const { return _sender.bytes_in_flight(); }
 
-size_t TCPConnection::unassembled_bytes() const { return {}; }
+size_t TCPConnection::unassembled_bytes() const { return _receiver.unassembled_bytes(); }
 
-size_t TCPConnection::time_since_last_segment_received() const { return {}; }
+size_t TCPConnection::time_since_last_segment_received() const { return _time_since_last_segment_received; }
 
-void TCPConnection::segment_received(const TCPSegment &seg) { DUMMY_CODE(seg); }
+void TCPConnection::segment_received(const TCPSegment &seg) {
+    if (!_active)
+        return;
+    _time_since_last_segment_received = 0;
 
-bool TCPConnection::active() const { return {}; }
+    // data segments with acceptable ACKs should be ignored in SYN_SENT
+    if (in_syn_sent() && seg.header().ack && seg.payload().size() > 0) {
+        return;
+    }
+    bool send_empty = false;
+    if (_sender.next_seqno_absolute() > 0 && seg.header().ack) {
+        // unacceptable ACKs should produced a segment that existed
+        if (!_sender.ack_received(seg.header().ackno, seg.header().win)) {
+            send_empty = true;
+        }
+    }
+
+    bool recv_flag = _receiver.segment_received(seg);
+    if (!recv_flag) {
+        send_empty = true;
+    }
+
+    if (seg.header().syn && _sender.next_seqno_absolute() == 0) {
+        connect();
+        return;
+    }
+
+    if (seg.header().rst) {
+        // RST segments without ACKs should be ignored in SYN_SENT
+        if (in_syn_sent() && !seg.header().ack) {
+            return;
+        }
+        unclean_shutdown(false);
+        return;
+    }
+
+    if (seg.length_in_sequence_space() > 0) {
+        send_empty = true;
+    }
+
+    if (send_empty) {
+        if (_receiver.ackno().has_value() && _sender.segments_out().empty()) {
+            _sender.send_empty_segment();
+        }
+    }
+    push_segments_out();
+}
+
+bool TCPConnection::active() const { return _active; }
 
 size_t TCPConnection::write(const string &data) {
-    DUMMY_CODE(data);
-    return {};
+    size_t ret = _sender.stream_in().write(data);
+    push_segments_out();
+    return ret;
 }
 
 //! \param[in] ms_since_last_tick number of milliseconds since the last call to this method
-void TCPConnection::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_last_tick); }
+void TCPConnection::tick(const size_t ms_since_last_tick) {
+    if (!_active)
+        return;
+    _time_since_last_segment_received += ms_since_last_tick;
+    _sender.tick(ms_since_last_tick);
+    if (_sender.consecutive_retransmissions() > TCPConfig::MAX_RETX_ATTEMPTS) {
+        unclean_shutdown(true);
+    }
+    push_segments_out();
+}
 
-void TCPConnection::end_input_stream() {}
+void TCPConnection::end_input_stream() {
+    _sender.stream_in().end_input();
+    push_segments_out();
+}
 
-void TCPConnection::connect() {}
+void TCPConnection::connect() {
+    // when connect, must active send a SYN
+    push_segments_out(true);
+}
 
 TCPConnection::~TCPConnection() {
     try {
         if (active()) {
-            cerr << "Warning: Unclean shutdown of TCPConnection\n";
-
             // Your code here: need to send a RST segment to the peer
+            cerr << "Warning: Unclean shutdown of TCPConnection\n";
+            unclean_shutdown(true);
         }
     } catch (const exception &e) {
         std::cerr << "Exception destructing TCP FSM: " << e.what() << std::endl;
     }
+}
+
+bool TCPConnection::push_segments_out(bool send_syn) {
+    // default not send syn before recv a SYN
+    _sender.fill_window(send_syn || in_syn_recv());
+    TCPSegment seg;
+    while (!_sender.segments_out().empty()) {
+        seg = _sender.segments_out().front();
+        _sender.segments_out().pop();
+        if (_receiver.ackno().has_value()) {
+            seg.header().ack = true;
+            seg.header().ackno = _receiver.ackno().value();
+            seg.header().win = _receiver.window_size();
+        }
+        if (_need_send_rst) {
+            _need_send_rst = false;
+            seg.header().rst = true;
+        }
+        _segments_out.push(seg);
+    }
+    clean_shutdown();
+    return true;
+}
+
+void TCPConnection::unclean_shutdown(bool send_rst) {
+    _receiver.stream_out().set_error();
+    _sender.stream_in().set_error();
+    _active = false;
+    if (send_rst) {
+        _need_send_rst = true;
+        if (_sender.segments_out().empty()) {
+            _sender.send_empty_segment();
+        }
+        push_segments_out();
+    }
+}
+
+bool TCPConnection::clean_shutdown() {
+    if (_receiver.stream_out().input_ended() && !(_sender.stream_in().eof())) {
+        _linger_after_streams_finish = false;
+    }
+    if (_sender.stream_in().eof() && _sender.bytes_in_flight() == 0 && _receiver.stream_out().input_ended()) {
+        if (!_linger_after_streams_finish || time_since_last_segment_received() >= 10 * _cfg.rt_timeout) {
+            _active = false;
+        }
+    }
+    return !_active;
+}
+
+bool TCPConnection::in_listen() { return !_receiver.ackno().has_value() && _sender.next_seqno_absolute() == 0; }
+
+bool TCPConnection::in_syn_recv() { return _receiver.ackno().has_value() && !_receiver.stream_out().input_ended(); }
+
+bool TCPConnection::in_syn_sent() {
+    return _sender.next_seqno_absolute() > 0 && _sender.bytes_in_flight() == _sender.next_seqno_absolute();
 }
